a228128805c5c726af2a05fb4548f334e73e9ed7
Bryce <bryce.embry@vumc.org>
2024-08-14 15:50:13 -0500

    Add doc blocks to DRY example

--- DRY/InstitutionService.php
+++ DRY/InstitutionService.php
@@ -4,6 +4,12 @@ namespace DRY;
 
 class InstitutionService
 {
+    /**
+     * This function will either fetch an existing Institution or create one if it doesn't exist
+     * @param string|null $fwa
+     * @param string|null $name
+     * @return InstitutionObject
+     */
     public function getOrCreateInstitution(?string $fwa, ?string $name): InstitutionObject
     {
         if (!is_null($fwa)) {
@@ -22,6 +28,16 @@ class InstitutionService
         return $institution;
     }
 
+    /**
+     * This function will create an Institution Object based on the given data
+     * @param string $fwa
+     * @param string $name
+     * @param string $address
+     * @param string $city
+     * @param string $state
+     * @param string $zip
+     * @return InstitutionObject
+     */
     public function createInstitution(
         string $fwa,
         string $name,
@@ -43,6 +59,17 @@ class InstitutionService
         return $institution;
     }
 
+    /**
+     * This function will update an Institution Object with the given values
+     * @param string $fwa
+     * @param string $name
+     * @param string $address
+     * @param string $city
+     * @param string $state
+     * @param string $zip
+     * @return InstitutionObject
+     * @throws \Exception
+     */
     public function updateInstitution(
         string $fwa,
         string $name,
@@ -67,11 +94,21 @@ class InstitutionService
         throw new \Exception('Invalid FWA');
     }
 
+    /**
+     * This function will retrieve an Institution Object by the FWA identifier
+     * @param string $value
+     * @return InstitutionObject|null
+     */
     public function getInstitutionByFwa(string $value): ?InstitutionObject
     {
         // Pretend there is search code here
     }
 
+    /**
+     * This function will retrieve an Institution Object by the name of tne institution
+     * @param string $name
+     * @return InstitutionObject|null
+     */
     public function getInstitutionByName(string $name): ?InstitutionObject
     {
         // Pretend there is search code here
--- DRY/notes.md
+++ DRY/notes.md
@@ -1,7 +1,8 @@
 # Don't Repeat Yourself
 
 ## Concepts
-* Centralize repeated code (like the "new InstitutionObject()" calls) 
+* Centralize repeated code (like the "new InstitutionObject()" calls)
 * Is it okay to reuse a variable name if the values change? 
+* Note that the Doc Blocks are not DRY - They repeat the information in the code
 * When have you over-generalized? Should getOrCreateInstitution() take two parameters, or should this be two different methods?
 * 
